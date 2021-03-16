8edd6a5fafcdfcf19fe44064d008e424c9ca9179
Soki_Naka <soki.run3.0@gmail.com>
2021-03-16 15:39:35 +0900

    Add ajax for follow

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Giftlog
 
     # タイムゾーンを日本時間に設定
     config.time_zone = 'Asia/Tokyo'
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
