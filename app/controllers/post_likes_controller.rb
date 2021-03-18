5c320ae3f01486e94047285496236976bfb8497c
Soki_Naka <soki.run3.0@gmail.com>
2021-03-18 16:54:17 +0900

    Add ajax for post-likes

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,11 +1,18 @@
 class PostLikesController < ApplicationController
+  before_action :post_params
   def create
     PostLike.create(user_id: current_user.id, post_id: params[:id])
-    redirect_back(fallback_location: @posts)
+    # redirect_back(fallback_location: @posts)
   end
 
   def destroy
     PostLike.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_back(fallback_location: @posts)
+    # redirect_back(fallback_location: @posts)
+  end
+
+  private
+
+  def post_params
+    @post = Post.find(params[:id])
   end
 end
