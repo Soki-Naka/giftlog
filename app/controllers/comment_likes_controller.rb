2ce7134c7e587d623f16582afd00cfcc39988b8d
Soki_Naka <soki.run3.0@gmail.com>
2021-03-28 15:22:29 +0900

    Add partial pages for comment-likes

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -2,12 +2,12 @@ class CommentLikesController < ApplicationController
   before_action :comment_params, only: %i[create destroy]
   def create
     CommentLike.create(user_id: current_user.id, comment_id: params[:id])
-    redirect_back(fallback_location: @post)
+    # redirect_back(fallback_location: @post)
   end
 
   def destroy
     CommentLike.find_by(user_id: current_user.id, comment_id: params[:id]).destroy
-    redirect_back(fallback_location: @post)
+    # redirect_back(fallback_location: @post)
   end
 
   private
