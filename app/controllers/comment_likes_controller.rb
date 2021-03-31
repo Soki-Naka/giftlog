90bd2df60d0024e07c810dde027f219474500098
Soki_Naka <soki.run3.0@gmail.com>
2021-03-31 09:59:23 +0900

    Finish ajax for comment-likes

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,5 +1,6 @@
 class CommentLikesController < ApplicationController
   before_action :comment_params, only: %i[create destroy]
+
   def create
     CommentLike.create(user_id: current_user.id, comment_id: params[:id])
   end
@@ -14,5 +15,3 @@ class CommentLikesController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 end
-
-# redirect_back(fallback_location: @post)
