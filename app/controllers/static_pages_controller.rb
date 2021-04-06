d14db3e5c7d423bdb32c40141446875e01a89e4b
Soki_Naka <soki.run3.0@gmail.com>
2021-04-06 13:19:27 +0900

    Add comment-likes ranking

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,13 +10,22 @@ class StaticPagesController < ApplicationController
     end
 
     # コメント回答数ランキング(自分の投稿へのコメントも含む)
-    @comments_users_rank = User.find(Comment.group(:user_id).order('count(user_id) desc').limit(3).pluck(:user_id))
+    # @comments_users_rank = User.find(Comment.group(:user_id).order('count(user_id) desc').limit(3).pluck(:user_id))
+
+    # コメント回答数ランキング(途中)
+    # posts = Post.arel_table
+    # comments = Comment.arel_table
+    # @comments_users_rank = User.joins(posts: :comments)
+    # .where.not(posts[:user_id].eq(comments[:user_id]))
+    # .group(posts[:user_id])
+    # .select('users.*, COUNT(`posts`.`id`) AS comments_count')
+    # .order('count(posts.user_id) DESC').limit(3)
 
     # コメントへのいいね獲得数ランキング
-    # @comments_likes_users_rank = User.joins(comments: :comment_likes).where(CommentLike.group(:user_id).order('count(user_id) desc').limit(3).pluck(:user_id))
-
-    # .group("liked_comments.id").order("count(liked_comments.user_id) desc")
-    # .sort {|a,b| b.liked_comments.size <=> a.liked_comments.size}
+    if logged_in?
+      comment_likes = CommentLike.arel_table
+      @comments_likes_users_rank = User.joins(comments: :comment_likes).where.not(comment_likes[:user_id].eq(current_user.id)).group(comment_likes[:comment_id]).select('users.*, COUNT(`comment_likes`.`id`) AS comments_count').order('count(comment_id) DESC').limit(3)
+    end
   end
 
   def about; end
