f823f0add12a3c3d093612c08ee9a3ee5200140c
Soki_Naka <soki.run3.0@gmail.com>
2021-05-03 17:22:15 +0900

    Modify form display

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,7 +2,8 @@ class Gift < ApplicationRecord
   belongs_to :favorite_person
   validates :favorite_person_id, presence: true
   validates :item, presence: true, length: { maximum: 100 }
-  validates :situation, length: { maximum: 30 }
+  validates :situation, length: { maximum: 50 }
+  validates :price, length: { maximum: 30 }
   validates :when, length: { maximum: 50 }
   validates :description, length: { maximum: 200 }
   default_scope -> { order(created_at: :desc) }
