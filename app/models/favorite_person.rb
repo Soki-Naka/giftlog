180f5b6842261312b5e1e5c9500aa3aca39afe3a
Soki_Naka <soki.run3.0@gmail.com>
2021-05-19 17:39:04 +0900

    大切な人リストに検索機能追加

--- app/models/favorite_person.rb
+++ app/models/favorite_person.rb
@@ -6,4 +6,9 @@ class FavoritePerson < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :description, length: { maximum: 200 }
   default_scope -> { order(created_at: :desc) }
+
+  # 名前による絞り込み
+  scope :get_by_name, lambda { |name|
+    where('name like ?', "%#{name}%")
+  }
 end
