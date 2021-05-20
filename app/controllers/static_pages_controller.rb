ff984ff31b7f86a558074e27e5d627082a3821d7
Soki_Naka <soki.run3.0@gmail.com>
2021-05-20 15:54:20 +0900

    コメントへのいいねランキングを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,41 @@ class StaticPagesController < ApplicationController
     else
       @posts = Post.all.page(params[:page]).per(5)
     end
+
+    # コメントへのいいね獲得数ランキング
+    # if logged_in?
+    #   comment_likes = CommentLike.arel_table
+    #   @comments_likes_users_rank = User.joins(comments: :comment_likes).where.not(comment_likes[:user_id].eq(current_user.id)).group(comment_likes[:comment_id]).select('users.*, COUNT(`comment_likes`.`id`) AS comments_count').order('count(comment_id) DESC').limit(3)
+    # end
+
+    comment_likes = CommentLike.arel_table
+    comments = Comment.arel_table
+    # これいじる↓
+    # posts = Post.arel_table
+
+    # コメント回答数ランキング(自分の投稿へのコメントも含む)
+    # @comments_users_rank = User.joins(posts: :comments).where.not(comments[:user_id].eq(posts[:user_id])).order('count(user_id) desc').limit(3).pluck(:user_id)
+    # @comments_users_rank = User.find(Comment.group(:user_id).order('count(user_id) desc').limit(3).pluck(:user_id))
+    # @comments_users_rank = User.find(Comment.group(:user_id).where.not(comments[:user_id].eq(posts[:user_id])).order('count(user_id) desc').limit(3).pluck(:user_id))
+    # これいじる↓
+    # @comments_users_rank = User.joins(posts: :comments).where.not(posts[:user_id].eq(comments[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(comments.id) DESC').limit(3)
+    #   @comments_users_rank = User.joins(posts: :comments)
+    # .where.not(posts[:user_id].eq(comments[:user_id]))
+    # .group(posts[:user_id])
+    # .select('users.*, COUNT(`comments`.`id`) AS comments_count')
+    # .order('count(posts.user_id) DESC').limit(3)
+
+    # @comments_users_rank = User.joins(posts: :comments).where.not(comments[:user_id].eq(posts[:user_id])).order('count(user_id) desc').limit(3)
+
+    # @comments_users_rank = User.find(Comment.group(:user_id).order(Arel.sql('count(user_id) desc')).limit(3).pluck(:user_id))
+    # @comments_users_rank = User.joins(posts: :comments).where.not(posts[:user_id].eq(comments[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(comments.id) DESC').limit(3)
+    # 良さげ？
+    # @comments_users_rank = User.joins(posts: :comments).where.not(posts[:user_id].eq(comments[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comment_likes`.`id`)').order('count(user_id) desc').limit(3).pluck(:user_id)
+
+    # @comments_users_rank = User.joins(comments: :posts).where.not(comments[:user_id].eq(posts[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comments`.`id`)').order('count(comment_id) DESC').limit(3)
+
+    # コメントへのいいね獲得数ランキング
+    @comments_likes_users_rank = User.joins(comments: :comment_likes).where.not(comment_likes[:user_id].eq(comments[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comment_likes`.`id`) AS comment_likes_count').order('count(comment_id) DESC').limit(3)
   end
 
   def about; end
