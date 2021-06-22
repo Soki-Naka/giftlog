74dcc3129a0d34372990a9822932461407e551d5
Soki_Naka <soki.run3.0@gmail.com>
2021-06-22 11:41:19 +0900

    非ログイン時のアイコンリンククリックによるエラーの解消

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -29,7 +29,10 @@ class StaticPagesController < ApplicationController
     # コメント回答数ランキング(自分の投稿へのコメントも含む)(修正版)↓
     # @comments_users_rank = User.joins(:comments).group(comments[:user_id]).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(user_id) desc').limit(3)
 
-    # @comments_users_rank = User.joins(posts: :comments).group(comments[:user_id]).having.not(comments[:user_id].eq(posts[:user_id])).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(comment_id) desc').limit(3)
+    # 5月26日↓
+    # @comments_users_rank = User.joins(:comments, :posts).group(comments[:user_id]).where.not(comments[:user_id].eq(posts[:user_id])).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(comments.id) desc').limit(3)
+
+    # @comments_users_rank = User.joins(:comments, :posts).group(comments[:user_id]).where.not(comments[:user_id].eq(posts[:user_id])).select('users.*, COUNT(`comments`.`id`) AS comments_count').order('count(comments.id) desc').limit(3)
 
     # コメントへのいいね獲得数ランキング
     @comments_likes_users_rank = User.joins(comments: :comment_likes).where.not(comment_likes[:user_id].eq(comments[:user_id])).group(comments[:user_id]).select('users.*, COUNT(`comment_likes`.`id`) AS comment_likes_count').order('count(comment_id) DESC').limit(3)
